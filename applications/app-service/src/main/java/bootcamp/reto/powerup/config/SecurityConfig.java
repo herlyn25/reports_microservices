ddb399f6e49de19fba9a2a96a0833656d48bd16d
herlyn25 <herly-1988@hotmail.com>
2025-09-19 23:35:10 -0500

    Agregando seguridad al endpoint /api/v1/reports con metodo GET

---
+++ applications/app-service/src/main/java/bootcamp/reto/powerup/config/SecurityConfig.java
@@ -0,0 +1,70 @@
+package bootcamp.reto.powerup.config;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtGrantedAuthoritiesConverterAdapter;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Value("${jwt.secret-key}")
+    private String secret;
+
+    private SecretKey secretKey;
+
+    @PostConstruct
+    void init() {
+        this.secretKey = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8), "HmacSHA256");
+    }
+
+    @Bean
+    ReactiveJwtDecoder jwtDecoder(){
+        return NimbusReactiveJwtDecoder.withSecretKey(secretKey).build();
+    }
+
+    // Convert "roles" claim -> ROLE_* for what functions hasRole("...")
+    private ReactiveJwtAuthenticationConverter jwtAuthConverter() {
+        var roles = new JwtGrantedAuthoritiesConverter();
+        roles.setAuthoritiesClaimName("roles");  // tu claim
+        roles.setAuthorityPrefix("ROLE_");       // requerido por hasRole
+
+
+        var converter = new ReactiveJwtAuthenticationConverter();
+        converter.setJwtGrantedAuthoritiesConverter(
+                new ReactiveJwtGrantedAuthoritiesConverterAdapter(roles)
+        );
+        return converter;
+    }
+
+    @Bean
+    SecurityWebFilterChain springSecurityPermissions(ServerHttpSecurity http, ReactiveJwtDecoder decoder) {
+        return http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(ex-> ex
+                        .pathMatchers(
+                                "/v3/api-docs/**",
+                                "/swagger-ui/**")
+                        .permitAll()
+                        .pathMatchers(HttpMethod.GET,"/api/v1/reports").hasRole("ADMIN")
+                        .anyExchange().authenticated())
+                .oauth2ResourceServer(oauth-> oauth
+                        .jwt(jwt->jwt.jwtDecoder(decoder)
+                                .jwtAuthenticationConverter(jwtAuthConverter())))
+                .build();
+    }
+}
---
+++ applications/app-service/src/main/java/bootcamp/reto/powerup/config/UseCasesConfig.java
@@ -0,0 +1,14 @@
+package bootcamp.reto.powerup.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(basePackages = "bootcamp.reto.powerup.usecase",
+        includeFilters = {
+                @ComponentScan.Filter(type = FilterType.REGEX, pattern = "^.+UseCase$")
+        },
+        useDefaultFilters = false)
+public class UseCasesConfig {
+}
