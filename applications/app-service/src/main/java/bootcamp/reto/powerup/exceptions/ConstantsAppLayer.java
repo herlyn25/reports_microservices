8f407c5114f8b1d7dcfcf6b9aa558eb20d80fdb8
herlyn25 <herly-1988@hotmail.com>
2025-09-22 19:29:02 -0500

    Excepciones y prepara proyecto para dockerización

---
+++ applications/app-service/src/main/java/bootcamp/reto/powerup/exceptions/ConstantsAppLayer.java
@@ -0,0 +1,8 @@
+package bootcamp.reto.powerup.exceptions;
+
+public class ConstantsAppLayer {
+    public static final String MESSAGE = "message";
+    public static final String ERROR = "error";
+    public static final String TIMESTAMP = "timestamp";
+    public static final String NO_FOUND_REPORT = "No found resource";
+}
---
+++ infrastructure/driven-adapters/dynamo-db/src/main/java/bootcamp/reto/powerup/dynamodb/exceptions/ConstantsException.java
@@ -0,0 +1,5 @@
+package bootcamp.reto.powerup.dynamodb.exceptions;
+
+public class ConstantsException {
+    public static final String NO_FOUND_RESOURCE = "No found resource report";
+}
